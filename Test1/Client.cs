898fd583b8a797dc461adafa84a26d85dd448fc0
Павел Андреев <paaha98@gmail.com>
2018-03-15 12:29:45 +0300

    Добавьте файлы проекта.

---
+++ Test1/Client.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test1
+{
+    class Client
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 1, 53, 4, 3, 41, 56, 45, 68, 15, 46, 64, 5, 6, 8, 51, 33, 54 };
+            printArray(array);
+            Shell.sort(array);
+            printArray(array);
+            Console.ReadKey();
+        }
+
+        static void printArray(int[] arr)
+        {
+            foreach (int i in arr)
+                Console.Write(i + " ");
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Test1/Shell.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test1
+{
+    class Shell
+    {
+        public static void sort(int[] arr)
+        {
+            int arrayLength = arr.Length;
+            int step = stepCalculation(arrayLength);
+
+            while (step > 0)
+            {
+                for (int i = step; i < arrayLength; i++)
+                    sortingInterval(arr, step, i);
+                step /= 2;
+            }
+        }
+
+        private static int stepCalculation(int arrayLength)
+        {
+            int step = (int)(Math.Log(arrayLength) / Math.Log(2)) - 1;
+            int h = 1;
+            for (int i = 0; i < step; i++)
+                h = h * 2 + 1;
+            return h;
+        }
+
+        private static void shifts(int[] array, int step, ref int index)
+        {
+            array[index + step] = array[index];
+            index -= step;
+        }
+
+        private static void sortingInterval(int[] array, int step, int index)
+        {
+            int a = array[index];
+            int j = index - step;
+            while (j >= 0 && array[j] > a)
+                shifts(array, step, ref j);
+            array[j + step] = a;
+        }
+    }
+}
